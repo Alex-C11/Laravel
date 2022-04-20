52dca266877ae6931218e32f245c2f2858ec750a
Alex-C11 <64272060+Alex-C11@users.noreply.github.com>
2022-04-20 10:51:16 -0500

    Laravel Alex
    Nada

---
+++ alpha/database/migrations/2022_04_20_134431_create_post_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_is');
+            $table->unsignedBigInteger('tag_is');
+            $table->foreign('post_is')->references('id')->on('posts')->onDelete('caascade');
+            $table->foreign('tag_is')->references('id')->on('tags')->onDelete('caascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+};
